c75ef107b2a0bdc3488017bb2cab73dc723733a4
Pablo Castillo <pablocastilloe@hotmail.com>
2014-06-01 00:31:46 -0500

    Se agrego como sincronizar con Github

--- archivo.js
+++ archivo.js
@@ -2,3 +2,8 @@ Estoy haciendo lo basico
 1)git ini     // Se utiliza para inicializar la carpeta .git que va a contener las versiones
 2)git add 'archivo'   // Se utiliza para anadir el archivo al control de versiones.
 3)gid commit -m "Descripcion Cambios"  // Recordar hacer los pasos 2 y 3 por cada version
+
+Para usar Github primero creamos nuevo proyecto de preferencia con el mismo nombre que la carpeta luego
+4)git remote add 'alias' htts://github.com/cuenta/carpeta.git
+Finalmente push
+5)git push -u 'alias' 'rama'  // el alias puesto en el paso 4 y la rama por lo general master (ojo sin comillas)
