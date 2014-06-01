f3b751898b7ac9904673ff6933983fc48f50c673
Pablo Castillo <pablocastilloe@hotmail.com>
2014-06-01 00:00:33 -0500

    Ahora habra dos versiones

--- archivo.js
+++ archivo.js
@@ -1,2 +1,4 @@
 Estoy haciendo lo basico
 1)git ini     // Se utiliza para inicializar la carpeta .git que va a contener las versiones
+2)git add 'archivo'   // Se utiliza para anadir el archivo al control de versiones.
+3)gid commit -m "Descripcion Cambios"  // Recordar hacer los pasos 2 y 3 por cada version
