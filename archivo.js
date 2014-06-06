cee76ce4f7dbb8ac287552b3c5fb7a72d771204f
Pablo Castillo <pablocastilloe@hotmail.com>
2014-06-05 19:40:56 -0500

    Hice un cambio para la linea 6

--- archivo.js
+++ archivo.js
@@ -4,6 +4,8 @@ Estoy haciendo lo basico
 3)gid commit -m "Descripcion Cambios"  // Recordar hacer los pasos 2 y 3 por cada version
 
 Para usar Github primero creamos nuevo proyecto de preferencia con el mismo nombre que la carpeta luego
-4)git remote add 'alias' htts://github.com/cuenta/carpeta.git
+4)git remote add 'alias' htts://github.com/cuenta/carpeta.git (Solo 1 vez)
 Finalmente push
 5)git push -u 'alias' 'rama'  // el alias puesto en el paso 4 y la rama por lo general master (ojo sin comillas)
+///////////////////////////////////////////////
+6) cambie algo
